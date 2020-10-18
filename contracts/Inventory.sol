095f31f95c4c69e19e5697ad0c07121bce06693a
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2020-10-18 12:00:51 -0300

    Update

--- contracts/Inventory.sol
+++ contracts/Inventory.sol
@@ -1,70 +1,67 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
+/**
+   Create and transfer ownership of items.
+   The functions I have in mind are:
+    - createItem # Creates a new item for the user
+    - buyItem    # Requests approval from the owner of the item to purchase the item
+ */
 contract Inventory {
     struct Item {
         string name;
         string description;
-        address creator;
+        uint256 price;
+        bool exists;
     }
 
-    uint256 counter = 1;
+    uint256 itemIdCounter = 1;
 
     mapping(uint256 => Item) items;
-    mapping(address => mapping(uint256 => bool)) items_by_address;
+    mapping(uint256 => address) itemOwners;
 
-    event ItemCreated(uint256 _item_id, address _creator);
-    event ItemTransfered(uint256 _item_id, address _from, address _to);
+    event ItemCreated(uint256 id);
 
-    function create_item(string memory _name, string memory _description)
-        public
-    {
-        _create_item(_name, _description);
-        _add_item_to_address(counter, msg.sender);
-        emit ItemCreated(counter, msg.sender);
-        counter++;
+    function createItem(
+        string memory name,
+        string memory description,
+        uint256 price
+    ) public {
+        Item storage item = items[itemIdCounter];
+        item.name = name;
+        item.description = description;
+        item.price = price;
+        item.exists = true;
+        itemOwners[itemIdCounter] = msg.sender;
+        emit ItemCreated(itemIdCounter);
+        itemIdCounter++;
     }
 
-    function transfer_item(uint256 _item_id, address _to) public {
-        require(has_item(_item_id));
-        _remove_item_from_address(_item_id, msg.sender);
-        _add_item_to_address(_item_id, _to);
-        emit ItemTransfered(_item_id, msg.sender, _to);
-    }
-
-    function has_item(uint256 _item_id) public view returns (bool) {
-        return items_by_address[msg.sender][_item_id];
-    }
-
-    function get_item(uint256 _item_id)
+    function getItem(uint256 id)
         public
         view
         returns (
             string memory,
             string memory,
+            uint256 price,
+            bool,
             address
         )
     {
-        Item memory _item = items[_item_id];
-        return (_item.name, _item.description, _item.creator);
+        Item memory item = items[id];
+        return (
+            item.name,
+            item.description,
+            item.price,
+            item.exists,
+            itemOwners[id]
+        );
     }
 
-    function _create_item(string memory _name, string memory _description)
-        private
-    {
-        Item storage item = items[counter];
-        item.name = _name;
-        item.description = _description;
-        item.creator = msg.sender;
-    }
-
-    function _add_item_to_address(uint256 _item_id, address _address) private {
-        items_by_address[_address][_item_id] = true;
-    }
-
-    function _remove_item_from_address(uint256 _item_id, address _address)
-        private
-    {
-        delete items_by_address[_address][_item_id];
+    function buyItem(uint256 id) public {
+        Item memory item = items[id];
+        require(item.exists);
+        address payable owner = payable(itemOwners[id]);
+        owner.transfer(item.price);
     }
 }
