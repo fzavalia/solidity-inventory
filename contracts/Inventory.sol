f8b9a02ed619a304141590c624e0e3bdb78bf469
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2020-10-19 23:12:45 -0300

    Create order when buying item

--- contracts/Inventory.sol
+++ contracts/Inventory.sol
@@ -8,12 +8,21 @@ contract Inventory {
         bool exists;
     }
 
+    struct Order {
+        uint256 itemId;
+        uint256 amount;
+        bool exists;
+    }
+
     uint256 itemIdCounter = 1;
+    uint256 orderIdCounter = 1;
 
     mapping(uint256 => Item) items;
     mapping(uint256 => address payable) itemOwners;
+    mapping(uint256 => Order) orders;
 
     event ItemCreated(uint256 id);
+    event OrderCreated(uint256 id);
 
     function getBalance() public view returns (uint256) {
         return address(this).balance;
@@ -42,5 +51,26 @@ contract Inventory {
         Item memory item = items[id];
         require(item.exists, "Item must exist");
         return (item.name, item.description, item.exists, itemOwners[id]);
+    }
+
+    function buyItem(uint256 id) public payable {
+        Item memory item = items[id];
+        require(item.exists, "Item must exist");
+        Order storage order = orders[orderIdCounter];
+        order.itemId = id;
+        order.amount = msg.value;
+        order.exists = true;
+        emit OrderCreated(orderIdCounter);
+        orderIdCounter++;
+    }
+
+    function getOrder(uint256 id)
+        public
+        view
+        returns (uint256 amount, uint256 itemId)
+    {
+        Order memory order = orders[id];
+        require(order.exists, "Order must exist");
+        return (order.amount, order.itemId);
     }
 }
--- test/inventory.js
+++ test/inventory.js
@@ -3,7 +3,8 @@ const Inventory = artifacts.require("Inventory");
 contract(Inventory, (accounts) => {
   it("should createItem", async () => {
     const inventory = await Inventory.deployed();
-    const result = await createItem(inventory, accounts[0]);
+    const args = { from: accounts[0] };
+    const result = await inventory.createItem("name", "desc", args);
     const log = result.logs[0];
     assert.equal(log.event, "ItemCreated");
     assert.equal(log.args.id, 1);
@@ -30,11 +31,30 @@ contract(Inventory, (accounts) => {
       assert.equal(item.owner, accounts[0]);
     });
   });
-});
 
-function createItem(inventory, account) {
-  return inventory.createItem("name", "desc", { from: account });
-}
+  describe("buyItem", () => {
+    it("should fail if item does not exist", async () => {
+      const inventory = await Inventory.deployed();
+      await shouldFail(inventory.buyItem(2), "Item must exist");
+    });
+
+    it("should create an order", async () => {
+      const inventory = await Inventory.deployed();
+      const args = { from: accounts[0], value: 10000 };
+      const result = await inventory.buyItem(1, args);
+      const log = result.logs[0];
+      assert.equal(log.event, "OrderCreated");
+      assert.equal(log.args.id, 1);
+    });
+  });
+
+  describe("getOrder", () => {
+    it("should fail if order does not exist", async () => {
+      const inventory = await Inventory.deployed();
+      await shouldFail(inventory.getOrder(2), "Order must exist");
+    });
+  });
+});
 
 async function shouldFail(promise, message) {
   let err;
